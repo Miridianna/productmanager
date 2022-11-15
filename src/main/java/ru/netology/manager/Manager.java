e343b6714371b1eca33a38be26baee72464b229b
Anna Yakovleva <akovlevaanna47@gmail.com>
2022-11-15 23:22:35 +0300

    Initial commit

---
+++ src/main/java/ru/netology/manager/Manager.java
@@ -0,0 +1,46 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Product;
+import ru.netology.repository.ProductRepository;
+
+public class Manager {
+    protected ProductRepository repo;
+
+    public Manager(ProductRepository repo) {
+        this.repo = repo;
+    }
+
+    public void add(Product product) {
+        repo.addProduct(product);
+    }
+
+    public void removeById(int id) {
+        repo.removeById(id);
+    }
+
+    public Product[] searchBy(String text) {
+        Product[] result = new Product[0];
+        for (Product product: repo.findAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                for (int i = 0; i < result.length; i++) {
+                    tmp[i] = result[i];
+                }
+                tmp[tmp.length - 1] = product;
+                result = tmp;
+            }
+        }
+        return result;
+    }
+
+    public boolean matches(Product product, String search) {
+        if (product.getName().contains(search)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    }
+
---
+++ src/test/java/RepositoryTest.java
@@ -0,0 +1,155 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
+import ru.netology.manager.Manager;
+import ru.netology.repository.ProductRepository;
+
+public class RepositoryTest {
+
+    Book book1 = new Book(
+            100,
+            "Testing",
+            1_000,
+            "Petrov");
+
+    Book book2 = new Book(
+            101,
+            "Testing and Programming",
+            2_000,
+            "Ivanov");
+
+    Smartphone phone1 = new Smartphone(
+            102,
+            "Motorola",
+            3_000,
+            "All Phones");
+
+    Smartphone phone2 = new Smartphone(
+            103,
+            "Nokia",
+            4_000,
+            "All Phones");
+
+    @Test
+    public void findAll() {
+        ProductRepository repo = new ProductRepository();
+
+        repo.addProduct(book1);
+        repo.addProduct(book2);
+        repo.addProduct(phone1);
+        repo.addProduct(phone2);
+
+        Product[] expected = { book1, book2, phone1, phone2 };
+        Product[] actual = repo.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeById() {
+        ProductRepository repo = new ProductRepository();
+
+        repo.addProduct(book1);
+        repo.addProduct(book2);
+        repo.addProduct(phone1);
+        repo.addProduct(phone2);
+
+        Product[] expected = { book2, phone1, phone2 };
+        Product[] actual = repo.removeById(book1.getId());
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void removeByIdInManagerIThoughtButNot() {
+        ProductRepository repo = new ProductRepository();
+        Manager manager = new Manager(repo);
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(phone1);
+        manager.add(phone2);
+
+        Product[] expected = { book1, book2, phone2 };
+        Product[] actual = repo.removeById(102);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void searchBy() {
+        ProductRepository repo = new ProductRepository();
+        Manager manager = new Manager(repo);
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(phone1);
+        manager.add(phone2);
+
+        Product[] expected = { book1, book2 };
+        Product[] actual = manager.searchBy("Testing");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void setProductById() {
+        Product product = new Product(100, "Testing", 1_000);
+
+        product.setId(100);
+        product.setName("Testing");
+        product.setPrice(1_000);
+
+        int expected = 100;
+        int actual = product.getId();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setProductByName() {
+        Product product = new Product(100, "Testing", 1_000);
+
+        product.setId(100);
+        product.setName("Testing");
+        product.setPrice(1_000);
+
+        String expected = "Testing";
+        String actual = product.getName();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setProductByPrice() {
+        Product product = new Product(100, "Testing", 1_000);
+
+        product.setId(100);
+        product.setName("Testing");
+        product.setPrice(1_000);
+
+        int expected = 1_000;
+        int actual = product.getPrice();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setBookByAuthor() {
+        Book book = new Book(101, "Testing and Programming", 2_000, "Ivanov");
+
+        book.setAuthor("Ivanov");
+
+        String expected = "Ivanov";
+        String actual = book.getAuthor();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void setSmartphoneByManufacturer() {
+        Smartphone phone = new Smartphone(102,"Motorola", 3_000, "All Phones" );
+
+        phone.setManufacturer("All Phones");
+
+        String expected = "All Phones";
+        String actual = phone.getManufacturer();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    }
